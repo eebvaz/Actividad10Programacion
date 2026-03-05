ad060a57fab6bc5b9d0903d42ef253efac883d1d
ALUMNO <caxajgabriel2@gmail.com>
2026-03-05 11:01:21 -0600

    Agregar archivos de proyecto.

---
+++ Programa1/Program.cs
@@ -0,0 +1,33 @@
+﻿
+
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+Dictionary<int, string> estudiantes = new Dictionary<int, string>();
+
+
+int id;
+int num;
+for (int i = 1; i <= 3; i++)
+{
+    Console.Write("Ingrese el ID ");
+    id = int.Parse(Console.ReadLine());
+    //Console.WriteLine("");
+    Console.Write("Ingrese el nombre del estudiante");
+    string nombre = Console.ReadLine();
+
+    estudiantes.Add(id, nombre);
+}
+
+Console.Clear();
+Console.Write("Ingrese el ID del estudiante que desea buscar: ");
+num = int.Parse(Console.ReadLine());
+if (num != estudiantes.Count)
+{
+    Console.Write("No se ha encontrado el usuario");
+}
+else
+{
+    Console.Write("Se ha encontrado: " + estudiantes);
+}
+
+
---
+++ Programa2/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+Dictionary<int, string> productos = new Dictionary<int, string>();
+
+
+int codigo,cantidad=0;
+string nombre;
+
+
+Console.WriteLine("Cantidad de productos: ");
+cantidad = int.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese el codigo de producto y su nombre");
+for (int i = 1; i <= cantidad; i++)
+{
+    Console.Write("Codigo: "); codigo = int.Parse(Console.ReadLine());
+    Console.Write("Nombre: "); nombre = Console.ReadLine();
+    productos.Add(codigo, nombre);
+}
+
+
+         
+
