255978b11888f58d2abefe6fe1863cb73867cdd4
eebvaz <ee.bvasquez@udeo.edu.gt>
2026-03-05 21:46:09 -0600

    Diesiocho listo

--- Programa18/Program.cs
+++ Programa18/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+
+
+Dictionary <string, string>  nombres  = new Dictionary<string, string>();
+
+nombres.Add("Ana","123");
+nombres.Add("Anny","456");
+
+int num=0;
+string usuario, contrasena;
+
+
+Console.WriteLine("Ingrese un usuario: ");
+usuario = Console.ReadLine();
+
+    if (!nombres.ContainsKey(usuario))
+    {
+        Console.WriteLine("Contraseña: ");
+        contrasena = Console.ReadLine();
+
+        nombres.Add(usuario, contrasena);
+
+    }
+    else
+    {
+        Console.WriteLine("Ya existe");
+    }
+
