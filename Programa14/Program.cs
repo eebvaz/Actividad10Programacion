93a5afb76b87eeaee4a050242812d6c514e4deff
eebvaz <ee.bvasquez@udeo.edu.gt>
2026-03-05 20:55:19 -0600

    Añadido

--- Programa14/Program.cs
+++ Programa14/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+
+Dictionary <int, string> ciudades  = new Dictionary<int, string>();
+
+ciudades.Add(3, "Cayala");
+ciudades.Add(2, "Capital");
+ciudades.Add(5, "Puerto san jose");
+ciudades.Add(6, "Villa nueva");
+ciudades.Add(1, "Mixco");
+ciudades.Add(4, "Tototnicapan");
+
+
+foreach  (var item in ciudades)
+{
+    Console.Write("  Codigos: "+item.Key);
+}
+Console.WriteLine("");
+foreach (var item in ciudades)
+{
+    Console.Write("  Ciudades: " + item.Value);
+}
