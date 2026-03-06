6f8556f925f6f282ea94bd2ea846bd28d4dd976f
eebvaz <ee.bvasquez@udeo.edu.gt>
2026-03-05 22:40:34 -0600

    Progreso

--- Programa19/Program.cs
+++ Programa19/Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+
+Dictionary <string, double> estudiante = new Dictionary<string, double>();
+
+
+int opcion =0;
+
+
+do
+{
+    Console.WriteLine("Menu");
+    Console.WriteLine("1. Añadir");
+    Console.WriteLine("2. Modificar");
+    Console.WriteLine("3. Mostrar");
+    Console.WriteLine("4. Salir");
+    opcion = int.Parse(Console.ReadLine());
+ 
+    switch(opcion)
+
+    {
+
+        case 1:
+            Console.WriteLine("Añadir");
+
+
+
+            Console.ReadKey();
+            break;
+
+            case 2:
+            Console.WriteLine("Modificar");
+
+
+
+            Console.ReadKey();
+            break;
+
+            case 3:
+            Console.WriteLine("Mostrar");
+
+
+
+            Console.ReadKey();
+            break;
+
+        default:
+
+            break;
+    }
+
+
+
+} while (opcion !=4);
