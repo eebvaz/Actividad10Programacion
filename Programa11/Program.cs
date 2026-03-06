121d262992607740fe5c300b2e71c4a5b2edccea
eebvaz <ee.bvasquez@udeo.edu.gt>
2026-03-05 20:32:37 -0600

    Onceavo añadido

--- Programa11/Program.cs
+++ Programa11/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+
+Dictionary <int, string> estudiantes  = new Dictionary<int, string>();
+
+estudiantes.Add(1, "Maria");
+estudiantes.Add(2, "Luis");
+estudiantes.Add(3, "Carlos");
+estudiantes.Add(4, "Marcos");
+estudiantes.Add(5, "Adrina");
+
+foreach (var item in estudiantes)
+{
+    Console.WriteLine("Estudiante: "+item.Value+ " Clave; "+ item.Key);
+}
