cec9700a0606f630ef271db11f05351fafb826bd
eebvaz <ee.bvasquez@udeo.edu.gt>
2026-03-05 19:17:06 -0600

    Septimo añadido

--- Programa7/Program.cs
+++ Programa7/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+
+using System.Numerics;
+
+Dictionary <int, string> productos  = new Dictionary<int, string>();
+
+productos.Add(1, "leche");
+productos.Add(2, "pan");
+
+int codigo;
+
+Console.WriteLine("Ingrese un codigo para verificar: ");
+codigo = int.Parse(Console.ReadLine());
+
+if(!productos.ContainsKey(codigo))
+{
+    Console.WriteLine("Ingrese un nuevo producto: ");
+}
+else
+{
+
+    Console.WriteLine("El producto ya existe " + productos[codigo]);
+}
