dc47ca3d6d96c3942a0cf7bbef78bc355be381ef
eebvaz <ee.bvasquez@udeo.edu.gt>
2026-03-05 18:35:32 -0600

    Cuarto programa añadido

--- Programa4/Program.cs
+++ Programa4/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+
+Dictionary <int , string> ciudades = new Dictionary<int , string> ();
+
+ciudades.Add(1, "Xela");
+ciudades.Add(2, "Quiche");
+ciudades.Add(3, "Totonicapan");
+ciudades.Add(4, "San antonio");
+ciudades.Add(5, "Puerto barrios");
+
+int codigo;
+
+Console.Write("Ingrese una ciudad del 1 al 5: ");
+codigo = int.Parse(Console.ReadLine());
+
+if (ciudades.ContainsKey(codigo))
+{
+    Console.WriteLine("La ciudad es " + ciudades[codigo]);
+}
+else
+{
+    Console.WriteLine("La ciudad no existe");
+}
