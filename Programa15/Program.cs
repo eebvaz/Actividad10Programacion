54e1da3455a780f83625a20a393dd414c6e4b518
eebvaz <ee.bvasquez@udeo.edu.gt>
2026-03-05 21:11:13 -0600

    Quince añadido

--- Programa15/Program.cs
+++ Programa15/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+
+Dictionary <int, string> empleados = new Dictionary<int, string>();
+
+empleados.Add(01, "Pedro");
+empleados.Add(02, "Anny");
+empleados.Add(03, "Camila");
+empleados.Add(04, "Pual");
+empleados.Add(05, "Saul");
+
+
+foreach  (var item in empleados)
+{
+
+    Console.WriteLine("ID;____"+item.Key+ "___"+"Nombre:___"+item.Value+"____");
+}
