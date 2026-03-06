5aeb2de157bc04168aff1873b8b27f6b700690d0
eebvaz <ee.bvasquez@udeo.edu.gt>
2026-03-05 19:10:38 -0600

    Sexto programa añadido

--- Programa6/Program.cs
+++ Programa6/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+
+Dictionary <int ,string> estudiantes = new Dictionary<int ,string>();
+
+
+estudiantes.Add(1, "Mario");
+estudiantes.Add(2, "Anni");
+estudiantes.Add(3, "Pablo");
+
+int codigo;
+
+Console.WriteLine("Ingrese el codigo del alumno a eliminar: ");
+codigo = int.Parse(Console.ReadLine());
+
+estudiantes.Remove(codigo);
+
+foreach  (var itema in estudiantes)
+{
+    Console.WriteLine(itema);
+}
