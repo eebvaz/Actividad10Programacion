489e6d73b2c98039aee96c2b906f81d95c7ec7c0
eebvaz <ee.bvasquez@udeo.edu.gt>
2026-03-05 19:28:48 -0600

    Octavo programa añadido

--- Programa8/Program.cs
+++ Programa8/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+
+Dictionary <int, string> empleados = new Dictionary<int, string>();
+
+
+empleados.Add(1, "Carlso");
+empleados.Add(2, "Anna");
+empleados.Add(3, "Mateo");
+empleados.Add(4, "Martin");
+
+
+int codigo;
+string nombre;
+
+foreach (var item in empleados)
+{
+    Console.WriteLine(item);
+}
+
+Console.WriteLine("");
+
+
+Console.WriteLine("Ingrese un codigo:  ");
+codigo = int.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese el nuevo nombre: ");
+nombre = Console.ReadLine();
+
+
+empleados[codigo] = nombre;
+
+foreach (var item in empleados)
+{
+    Console.WriteLine(item.Key + " " + item.Value);
+}
