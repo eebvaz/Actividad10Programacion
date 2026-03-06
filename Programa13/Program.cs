78610f31df1d25625c7cee30e1251c6f49352280
eebvaz <ee.bvasquez@udeo.edu.gt>
2026-03-05 20:46:26 -0600

    treceavo listo

--- Programa13/Program.cs
+++ Programa13/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+
+Dictionary <string , double> estudiantes  = new Dictionary<string , double>();
+
+estudiantes.Add("Ana",12);
+estudiantes.Add("Anita",54);
+estudiantes.Add("Lupe",76);
+estudiantes.Add("Laura",79);
+estudiantes.Add("Carlos",40);
+estudiantes.Add("Manuel",100);
+estudiantes.Add("Pedro",34);
+
+
+double total;
+double suma =0;
+
+foreach (var item in estudiantes)
+{
+   suma += item.Value;
+}
+
+total = suma/estudiantes.Count;
+
+Console.WriteLine("El promedio es: "+ total);
