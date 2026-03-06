33712c1e46df3016af2d6c9919ff19cd5018e7bf
eebvaz <ee.bvasquez@udeo.edu.gt>
2026-03-05 20:20:41 -0600

    Noveno listo

--- Programa9/Program.cs
+++ Programa9/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+
+
+Dictionary <string, double> estudiantes = new Dictionary<string, double>();
+
+estudiantes.Add("Martin", 54);
+estudiantes.Add("Laura", 60);
+estudiantes.Add("Mario", 34);
+estudiantes.Add("Luis", 66);
+estudiantes.Add("Jorgito", 85);
+
+
+string nombre;
+
+Console.WriteLine("Ingrese el nombre del estudiante: ");
+nombre  = Console.ReadLine();
+
+if (estudiantes.ContainsKey(nombre))
+{
+    Console.WriteLine("Existe");
+    Console.WriteLine("Nombre: "+nombre+" Nota: "+ estudiantes[nombre] );
+}
+else
+{
+
+    Console.WriteLine("No existe");
+
+}
