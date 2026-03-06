98b2e7e0aa443980aa6d47927b050acea26ecd57
eebvaz <ee.bvasquez@udeo.edu.gt>
2026-03-05 20:37:06 -0600

    Doceavo listo

--- Programa12/Program.cs
+++ Programa12/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+Dictionary <int, string> productos  = new Dictionary <int, string> ();
+
+productos.Add(10, "Maiz");
+productos.Add(11, "Calabaza");
+productos.Add(12, "Caña");
+productos.Add(13, "Paja");
+productos.Add(14, "Trigo");
+productos.Add(15, "Arroz");
+
+
+foreach (var item in productos)
+{
+    Console.WriteLine("codigo: "+item.Key +  " producto: "+item.Value);
+}
+
+
+
+
+
+
