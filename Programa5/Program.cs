b7a5963d30bb0b24f45c6e1c1a8adba32288d0b0
eebvaz <ee.bvasquez@udeo.edu.gt>
2026-03-05 19:04:41 -0600

    Quinto programa añadido

--- Programa5/Program.cs
+++ Programa5/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+Dictionary <string, string> constrasena = new Dictionary<string, string>();
+
+string usuario;
+string contra;
+
+
+
+for (int i = 1; i <= 3; i++)
+{
+Console.Write("Ingrese un usuario: ");
+usuario = Console.ReadLine();
+Console.Write("Ingrese una constraseña: ");
+contra = Console.ReadLine();
+
+  constrasena.Add(usuario, contra);
+}
+
+Console.Clear();
+
+Console.Write("Ingrese su usuario: ");
+usuario = Console.ReadLine();
+
+
+if (constrasena.ContainsKey(usuario))
+{
+    Console.Write("Usuario correcto");
+    Console.Write("Ingrese su constrasela: ");
+    contra = Console.ReadLine();
+
+    if (constrasena.ContainsKey(contra))
+    {
+        Console.Write("Contraseña correcta");
+    }
+    else
+    {
+        Console.Write("Contraseña incorrecta");
+    }
+
+}
+else
+{
+    Console.Write("No existe ese usuario");
+}
+
+
