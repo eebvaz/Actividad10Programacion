a5802e0ca5b3f32ef50669f954c58d72e4e603fa
eebvaz <ee.bvasquez@udeo.edu.gt>
2026-03-08 21:34:06 -0600

    Numera veinte lista

--- Programa20/Program.cs
+++ Programa20/Program.cs
@@ -1,17 +1,17 @@
 ﻿
-Dictionary<string, double> estudiante = new Dictionary<string, double>();
+Dictionary<string,(string, int , string)> estudiante = new Dictionary<string, (string, int, string)>();
 
 
 int opcion = 0;
-
-
+int cantidad, edad;
+string nombre, carrera, carnet;
 do
 {
+    Console.Clear();
     Console.WriteLine("Menu");
     Console.WriteLine("1. Añadir");
-    Console.WriteLine("2. Modificar");
-    Console.WriteLine("3. Mostrar");
-    Console.WriteLine("4. Salir");
+    Console.WriteLine("2. Mostrar");
+    Console.WriteLine("3. Salir");
     opcion = int.Parse(Console.ReadLine());
 
     switch (opcion)
@@ -21,23 +21,42 @@ do
         case 1:
             Console.WriteLine("Añadir");
 
+            
+            for (int i = 1; i <=3; i++)
+
+            {
+                Console.Clear();
+                Console.WriteLine("Ingrese su carnet: ");
+                carnet = Console.ReadLine();
+             
+            Console.WriteLine("Ingrese un nombre: ");
+                nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese su edad: ");
+                edad = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese su carrera: ");
+                carrera = Console.ReadLine();
+
+
+                estudiante.Add(carnet, (nombre, edad, carrera));
+            }
 
 
             Console.ReadKey();
             break;
+
+ 
 
         case 2:
-            Console.WriteLine("Modificar");
+            Console.Clear();  Console.WriteLine("Mostrar");
 
-
-
-            Console.ReadKey();
-            break;
-
-        case 3:
-            Console.WriteLine("Mostrar");
-
-
+            foreach (var item in estudiante)
+            {
+                Console.WriteLine("Carné: " + item.Key);
+                Console.WriteLine("Nombre: " + item.Value.Item1);
+                Console.WriteLine("Edad: " + item.Value.Item2);
+                Console.WriteLine("Carrera: " + item.Value.Item3);
+                Console.WriteLine();
+            }
 
             Console.ReadKey();
             break;
@@ -49,4 +68,4 @@ do
 
 
 
-} while (opcion != 4);
+} while (opcion != 3);
