9283e8689d823ffc30c527213fd2849bdbd19f70
eebvaz <ee.bvasquez@udeo.edu.gt>
2026-03-08 21:18:39 -0600

    Diesinueve lista

--- Programa19/Program.cs
+++ Programa19/Program.cs
@@ -6,7 +6,8 @@ Dictionary <string, double> estudiante = new Dictionary<string, double>();
 
 int opcion =0;
 
-
+string nombre;
+double nota;
 do
 {
     Console.WriteLine("Menu");
@@ -23,15 +24,33 @@ do
         case 1:
             Console.WriteLine("Añadir");
 
+            Console.WriteLine("Ingrese el nombre: ");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese la nota: ");
+            nota = double.Parse(Console.ReadLine());
 
-
+            estudiante[nombre]= nota;
             Console.ReadKey();
             break;
 
             case 2:
             Console.WriteLine("Modificar");
+            nombre = Console.ReadLine();
+            Console.WriteLine("");
 
 
+            if(opcion == 1)
+            {
+                Console.WriteLine("ingrese un nueva nota: ");
+
+                estudiante[nombre] = double.Parse(Console.ReadLine());
+
+            }
+            else
+            {
+                Console.WriteLine("Estudiante no existe");
+            }
+               
 
             Console.ReadKey();
             break;
@@ -39,7 +58,10 @@ do
             case 3:
             Console.WriteLine("Mostrar");
 
-
+            foreach (var item in estudiante)
+            {
+                Console.WriteLine(item.Key+"  : "+ item.Value);
+            }
 
             Console.ReadKey();
             break;
